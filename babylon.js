918fe0de4638656ff27095a615880c3fba17e1cc
suzuru0611 <suzuru0611@gmail.com>
2022-12-13 20:52:36 +0800

    123
    123

--- babylon.js
+++ babylon.js
@@ -13,12 +13,18 @@ const createScene = function() {
     const camera = new BABYLON.ArcRotateCamera("Camera", alpha, beta, radius, target, scene);
     camera.attachControl(canvas, true);
     
-    const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(5, 5, 5));
+    const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(5, 5, 0));
     
-    const box = BABYLON.MeshBuilder.CreateBox("box", {});
-    box.position.x = 0.5;
-    box.position.y = 1;
+    const box01 = BABYLON.MeshBuilder.CreateBox("box", {width:2,height:1.5,depth:3});
+    box01.position.x = 0;
+    box01.position.y = 0;
+    const roof01 = BABYLON.MeshBuilder.CreateCylinder('roof',{diameter:1.2,height:1.2,tessellation:3})
+    roof01.position.y =1;
+    roof01.rotation.z=Math.PI/2
     
+    const groundMat = new BABYLON.StandardMaterial('groundMat')
+    groundMat.diffuseColor = new BABYLON.Texture("https://www.babylonjs-playground.com/textures/floor.png");
+    box01.material = groundMat;
     return scene;
 };
 const sceneToRender = createScene();

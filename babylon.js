ec50e9d19ab91e48d3fab10ed93d3ff84157f50b
suzuru0611 <suzuru0611@gmail.com>
2022-12-23 19:17:21 +0800

    Update babylon.js

--- babylon.js
+++ babylon.js
@@ -8,7 +8,7 @@ const createScene = () => {
     const scene = new BABYLON.Scene(engine);
     scene.clearColor = new BABYLON.Color4(0.8, 0.8, 0.8);
 
-    const camera = new BABYLON.ArcRotateCamera("camera", Math.PI / 2, Math.PI / 2.5, 5, new BABYLON.Vector3(0, 0, 0));
+    const camera = new BABYLON.ArcRotateCamera("camera", Math.PI / 2, Math.PI / 2, 8, new BABYLON.Vector3(0, 1, 0));
     camera.attachControl(canvas, true);
     const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(5, 2, 5)); 
 

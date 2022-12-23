9b025ddad5bcf5c9fa9bce1cb5bfa71aefcb9e16
suzuru0611 <suzuru0611@gmail.com>
2022-12-23 19:09:55 +0800

    221223

--- babylon.js
+++ babylon.js
@@ -6,12 +6,13 @@ const engine = new BABYLON.Engine(canvas, true);
 const createScene = () => {
     
     const scene = new BABYLON.Scene(engine);
+    scene.clearColor = new BABYLON.Color4(0.8, 0.8, 0.8);
 
-    const camera = new BABYLON.ArcRotateCamera("camera", Math.PI / 2, Math.PI / 2.5, 10, new BABYLON.Vector3(0, 0, 0));
+    const camera = new BABYLON.ArcRotateCamera("camera", Math.PI / 2, Math.PI / 2.5, 5, new BABYLON.Vector3(0, 0, 0));
     camera.attachControl(canvas, true);
-    const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0)); 
+    const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(5, 2, 5)); 
 
-    BABYLON.SceneLoader.Append("", "seeeefood.glb", scene, function (scene) {
+    BABYLON.SceneLoader.Append("", "seeeeaFood.glb", scene, function (scene) {
         // do something with the scene
     });
 
